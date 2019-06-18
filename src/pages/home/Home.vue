5473b8f5a3f49dd6250ebd1e115464ced3b56745
张涛 <zhangtaos@yonyou.com>
2019-06-18 15:16:03 +0800

    commit Signed-off-by: 张涛 <zhangtaos@yonyou.com>

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,56 @@
 <template>
-   <home-header></home-header>
+  <div>
+   <home-header :city="city"></home-header>
+   <home-swiper :list="swiperList"></home-swiper>
+   <home-icons :list="iconList"></home-icons>
+   <home-recommend :list="recommendList"></home-recommend>
+   <home-weekend :list="weekendList"></home-weekend>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 import HomeHeader from './components/HomeHeader'
+import HomeSwiper from './components/HomeSwiper'
+import HomeIcons from './components/HomeIcons'
+import HomeRecommend from './components/HomeRecommend'
+import HomeWeekend from './components/HomeWeekend'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  },
+  data () {
+    return {
+      city: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = data.city
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+    }
   }
 }
 </script>
---
+++ src/pages/search/City.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <city-header></city-header>
+      <city-search></city-search>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/CityHeader'
+import CitySearch from './components/CitySearch'
+export default {
+  name: 'Search',
+  components: {
+    CityHeader,
+    CitySearch
+  }
+}
+</script>
+
+<style>
+
+</style>
