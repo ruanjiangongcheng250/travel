046053b28e602da5baa546e8cb5627dad34c2218
张涛 <zhangtaos@yonyou.com>
2019-06-20 09:25:33 +0800

    '1111'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <city-header></city-header>
-      <city-search></city-search>
+      <city-search :cities="cities"></city-search>
       <city-list :cities="cities" :hot="hot" :letter="letter"></city-list>
       <city-alphabet @change="handleLetterChange" :cities="cities"></city-alphabet>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
-import City from '@/pages/search/City'
+import City from '@/pages/city/City'
 
 Vue.use(Router)
 
